a11db3c82a3035920bf517ed29f5912062ee9578
Juan Carlos Navarro Velazquez <navel140494@gmail.com>
2019-03-27 18:20:35 -0600

    Modulo de bibo_reporte_unificado en la version odoo10

---
+++ bibo-reporte-unificado/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Part of AppJetty. See LICENSE file for full copyright and licensing details.
---
+++ bibo-reporte-unificado/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Bibo Reporte Operación unificado',
+    'version': '1.0',
+    'summary': 'Personalizacion de bibo',
+    'description': 'Modificacion del reporte de operaciones',
+    'category': 'Personalizacion',
+    'author': 'Xmarts',
+    'website': 'www.xmarwdwedtsfgh.com',
+    'depends': [
+        'mrp',
+                ],
+    'data': [
+        'reports/report_operacion_unificado.xml',
+        'reports/reports_menu.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
